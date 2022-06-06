0f7a9bc102e7c4ea548c1d7d2dbd544868d45389
AhmadyarRashid <ahmadyarmeo@gmail.com>
2022-06-06 12:33:34 +0500

    display slider

--- src/App.js
+++ src/App.js
@@ -8,31 +8,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      {/*<button*/}
-      {/*  tabIndex={-1}*/}
-      {/*  style={{*/}
-      {/*    cursor: 'pointer',*/}
-      {/*    padding: 8,*/}
-      {/*    background: 'green',*/}
-      {/*    color: 'white',*/}
-      {/*    marginBottom: 8,*/}
-      {/*    marginTop: 8,*/}
-      {/*    borderRadius: 8,*/}
-      {/*    border: 'unset'*/}
-      {/*  }}*/}
-      {/*  onClick={handle.enter}*/}
-      {/*>Full ScreenMode</button>*/}
-      {/*<FullScreen handle={handle}>*/}
-      {/*  <SimpleImageSlider*/}
-      {/*    width="100%"*/}
-      {/*    height="100%"*/}
-      {/*    images={images}*/}
-      {/*    loop*/}
-      {/*    autoPlay*/}
-      {/*   showBullets*/}
-      {/*  />*/}
-      {/*</FullScreen>*/}
+      <button
+        tabIndex={-1}
+        style={{
+          cursor: 'pointer',
+          padding: 8,
+          background: 'green',
+          color: 'white',
+          marginBottom: 8,
+          marginTop: 8,
+          borderRadius: 8,
+          border: 'unset'
+        }}
+        onClick={handle.enter}
+      >Full ScreenMode</button>
+      <FullScreen handle={handle}>
+        <SimpleImageSlider
+          width="100%"
+          height="100%"
+          images={images}
+          loop
+          autoPlay
+         showBullets
+        />
+      </FullScreen>
     </div>
   );
 }
